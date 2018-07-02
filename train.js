0f6bade099835b757ad788f4e7d9aa15c61916e2
Francisco Diaz <sisco@Franciscos-MacBook-Pro.local>
2018-07-02 01:30:11 -0700

    added js

---
+++ train.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyACjn9nVChqctAAIfhygDj-vVl6DZ0d_VU",
+    authDomain: "train-hub.firebaseapp.com",
+    databaseURL: "https://train-hub.firebaseio.com",
+    projectId: "train-hub",
+    storageBucket: "",
+    messagingSenderId: "330138126219"
+};
+firebase.initializeApp(config);
